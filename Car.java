19da96fda28358a5a50a4cbdb3f5057c26c2fac3
Ninjice <31352846+ninjice@users.noreply.github.com>
2017-11-03 08:54:33 -0700

    11/2

---
+++ Car.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Car here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Car
+{
+    // instance variables - replace the example below with your own
+    private String model;
+    private int year;
+
+    /**
+     * Constructor for objects of class Car
+     */
+    public Car(String mdl, int yr)
+    {
+        // initialise instance variables
+        this.model = mdl;
+        this.year = yr;
+    }
+    public String toString(){
+        return "This is a " + model + " built in " + year;
+    }
+    public boolean equals(Car otherCar){
+        if(this.model.equals(otherCar.model) && (this.year == otherCar.year)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
